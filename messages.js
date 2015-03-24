6a8d86c8a1bf91cc69007a9c8fb030be266da99c
devops001 <devops001@gmail.com>
2015-03-24 14:33:54 -0700

    more messages

--- messages.js
+++ messages.js
@@ -3,11 +3,23 @@ var Messages = {};
 
 Messages.Ascetic = [
   "hail the goer, friend",
-  "I am overpowered by craving...",
   "be heedful or you shall never escape",
-  "abandon conceit!",
+  "abandon conceit",
   "most are not ready for the contemplative life",
-  "I haven't eaten in several days yet I am still contented",
-  "move along lowly one!",
-  "you have nothing that I desire"
+  "move along, there is nothing for you here",
+  "you have nothing that I desire",
+  "ha!, they call that a tree",
+  "be ever mindful!",
+  "be here now",
+  "let go and go forth",
+  "abandon craving",
+  "a life gone forth is a life worth living",
+  "around and around we go",
+  "well met, friend",
+  "don't fetter yourself with evil deeds or evil thoughts",
+  "the sage finds delight in practice",
+  "have you prepared to meet the End-maker?",
+  "what will you take with you when you go?",
+  "what follows behind you like a shadow and never leaves?",
+  "where will you arise again?"
 ];
